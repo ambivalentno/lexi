5ac0c91a8ebe4f0f45c29cf5f1face0db889bee8
nikita <khlestovnikita@gmail.com>
2014-04-05 22:45:08 +0300

    dropped local.py from index, works with foreman

--- lexi/apps/lection/admin.py
+++ lexi/apps/lection/admin.py
@@ -1,56 +1,56 @@
 from django.contrib import admin
-from django.utils.translation import ugettext_lazy as _
+# from django.utils.translation import ugettext_lazy as _
 
-from embed_video.admin import AdminVideoMixin
-from nested_inlines.admin import NestedModelAdmin, NestedStackedInline, NestedTabularInline
+# from embed_video.admin import AdminVideoMixin
+# from nested_inlines.admin import NestedModelAdmin, NestedStackedInline, NestedTabularInline
 
-from .models import Course, Lesson, Question, Answer, Unit
-
-
-class AnswerInline(NestedTabularInline):
-    model = Answer
-    extra = 3
+# from .models import Course, Lesson, Question, Answer, Unit
 
 
-class QuestionInline(NestedStackedInline):
-    model = Question
-    inlines = [AnswerInline,]
-    extra = 1
-    fieldsets = [
-        (_('Text'), {'fields': ['text']}),
-        (_('Additional data'),
-         {'fields': ['title'],
-         'classes': ['collapse']}
-        ),
-    ]
+# class AnswerInline(NestedTabularInline):
+#     model = Answer
+#     extra = 3
 
 
-class UnitInline(NestedStackedInline):
-    model = Unit
-    inlines = [QuestionInline,]
-    extra = 1
-    fieldsets = [
-        (_('Data'), {'fields': ['title', 'video']}),
-        (_('Explanation'), {'fields': ['explanation'], 'classes': ['collapse']}),
-    ]
+# class QuestionInline(NestedStackedInline):
+#     model = Question
+#     inlines = [AnswerInline,]
+#     extra = 1
+#     fieldsets = [
+#         (_('Text'), {'fields': ['text']}),
+#         (_('Additional data'),
+#          {'fields': ['title'],
+#          'classes': ['collapse']}
+#         ),
+#     ]
 
 
-class UnitAdmin(AdminVideoMixin, NestedModelAdmin):
-    inlines = [QuestionInline]
+# class UnitInline(NestedStackedInline):
+#     model = Unit
+#     inlines = [QuestionInline,]
+#     extra = 1
+#     fieldsets = [
+#         (_('Data'), {'fields': ['title', 'video']}),
+#         (_('Explanation'), {'fields': ['explanation'], 'classes': ['collapse']}),
+#     ]
 
 
-class LessonAdmin(AdminVideoMixin, NestedModelAdmin):
-    inlines = [UnitInline]
-    fields = ('title', 'course')
+# class UnitAdmin(AdminVideoMixin, NestedModelAdmin):
+#     inlines = [QuestionInline]
 
 
-class UserAnsAdmin(admin.ModelAdmin):
-    fields = (
-        'user',
-        'answer',
-    )
-    # pass
+# class LessonAdmin(AdminVideoMixin, NestedModelAdmin):
+#     inlines = [UnitInline]
+#     fields = ('title', 'course')
 
-admin.site.register(Unit, UnitAdmin)
-admin.site.register(Course)
-admin.site.register(Lesson, LessonAdmin)
+
+# class UserAnsAdmin(admin.ModelAdmin):
+#     fields = (
+#         'user',
+#         'answer',
+#     )
+#     # pass
+
+# admin.site.register(Unit, UnitAdmin)
+# admin.site.register(Course)
+# admin.site.register(Lesson, LessonAdmin)
--- lexi/settings/base.py
+++ lexi/settings/base.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -62,22 +62,13 @@ MEDIA_ROOT = root('media')
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = '/media/'
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = root('static')
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
+# heroku staticfiles config.
+# Taken from https://devcenter.heroku.com/articles/getting-started-with-django
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-
-# Additional locations of static files
 STATICFILES_DIRS = (
-    root('static/common'),
-# Put strings here, like "/home/html/static" or "C:/www/django/static".
-# Always use forward slashes, even on Windows.
-# Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -110,7 +101,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'users.avatars.CustomSocialAuthExceptionMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
     )
@@ -128,7 +118,6 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'nested_inlines',
     'django.contrib.admin',
     'django.contrib.admindocs',
 
@@ -137,7 +126,6 @@ INSTALLED_APPS = (
 
     'south',
     'django_extensions',
-    'social_auth',
     'django_behave',
 
     )
@@ -181,8 +169,20 @@ QUESTIONPIC_SIZES = (
     (400, 300),
 )
 
+# # Allow all host hosts/domain names for this site
+ALLOWED_HOSTS = ['*']
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+
+DATABASES = { 'default' : dj_database_url.config()}
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
 try:
-    from .local import *
+    from .local_settings import *
 except:
     print 'please add local.py'
 
