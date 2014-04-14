fe0f810088c4103e02497154c877d8bcccc1b1ce
nikita <khlestovnikita@gmail.com>
2014-04-15 01:17:47 +0200

    dropped social auth and added downtime app

--- lexi/settings/heroku.py
+++ lexi/settings/heroku.py
@@ -5,4 +5,19 @@ from .utils import *
 from .users import *
 
 SECRET_KEY = os.environ['SECRET_KEY']
-DEBUG = os.environ['DEBUG']
+DEBUG = os.environ.get('DEBUG') or False
+
+INSTALLED_APPS += (
+    'downtime',
+)
+
+MIDDLEWARE_CLASSES = (
+     'downtime.middleware.DowntimeMiddleware',
+) + MIDDLEWARE_CLASSES
+
+MAINTENANCE_FILE = root('../etc/maintenance')
+
+DOWNTIME_EXEMPT_PATHS = (
+    '/admin',
+    '/static',
+)
